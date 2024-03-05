0a87aa943f22c5283931f486111fde9eb4ab5156
325r23 <r2332@qq.com>
2024-03-05 23:37:31 +0800

    3242

---
+++ test222.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify  
+import json  
+from flask_cors import CORS
+app = Flask(__name__)  
+# 初始化 CORS，并设置允许所有源进行访问
+CORS(app, resources={r"/*": {"origins": "*"}})
+@app.route('/register', methods=['POST'])  
+def register():
+    data = {
+		"a":1213,
+		"b":3346
+    }
+    return jsonify(data)
+  
+if __name__ == '__main__':  
+    app.run(debug=True)
